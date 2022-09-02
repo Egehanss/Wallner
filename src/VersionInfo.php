7152ffa63dbd75b371dd79362ebd9f1e5a3e08ea
FurkanYks <52335533+FurkanYks@users.noreply.github.com>
2022-09-03 02:46:54 +0300

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -35,7 +35,7 @@ final class VersionInfo{
 	public const BASE_VERSION = "4.8.2";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
-	public const WALLNER_VERSION = "1.0.2";
+	public const WALLNER_VERSION = "1.0.3";
 
 	private function __construct(){
 		//NOOP
