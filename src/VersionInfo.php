a521505ad16f133e6bc5e746717b98ac6ddcf7ca
FurkanYks <52335533+FurkanYks@users.noreply.github.com>
2022-09-05 21:48:47 +0300

    1.0.6

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -35,10 +35,10 @@ final class VersionInfo{
 	public const BASE_VERSION = "4.8.2";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
-	public const WALLNER_VERSION = "1.0.5";
+	public const WALLNER_VERSION = "1.0.6";
 
 	private function __construct(){
-		//NOOP
+		
 	}
 
 	private static ?string $gitHash = null;
